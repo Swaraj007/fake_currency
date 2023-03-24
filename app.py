d8a6f5b082e40e86513c6fd4fcbbefb942482ae0
Swaraj007 <84506916+Swaraj007@users.noreply.github.com>
2023-03-24 21:19:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import numpy as np
 from keras.models import load_model
 from PIL import Image
 
+
+st.set_page_config(
+    page_title="Fake Currency Detection",
+    page_icon=":smiley:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+
 st.sidebar.title("Fake Currency Detection")
 st.sidebar.image("s1.jpg")
 
@@ -30,12 +40,7 @@ def fake_currency(imgg):
     prediction = model.predict(img)
     Fake=np.argmax(prediction)
 
-    if Fake == 0:
-        cd="No Fake Currency detected"
-
-    elif Fake == 1:
-        cd="Fake Currency detected"
-    return cd
+    return Fake
 
 def hash_password(password):
     """Hash a password using SHA-256"""
@@ -75,7 +80,7 @@ def login():
             session_id = user[0] # Use the username as the session ID
             st.session_state['session_id'] = session_id
         
-                
+            st.experimental_rerun()
         else:
             st.error("Incorrect username or password")
    
@@ -85,15 +90,37 @@ def get_image():
         st.image(img, width=500)  
         result = fake_currency(img)
         
-        st.header(result)
+        
+        if result == 0:
+            st.markdown('<p style="color:lime; font-size: 30px;font-weight:bold">No Fake Currency detected</p>', unsafe_allow_html=True)
+        elif result == 1:
+            st.markdown('<p style="color:red; font-size: 30px;font-weight:bold">Fake Currency detected</p>', unsafe_allow_html=True)
 
 def logout():
     st.session_state.pop('session_id', None)
-    st.info("You have been logged out")  
+    st.info("You have been logged out") 
+    st.experimental_rerun() 
     
-menu = ["Upload Image","Signup","Login", "Logout"]
+def about():
+    
+    para = '''<p>
+    Fake currency detection is an important task in the field of image processing and computer vision. With the increasing availability of high-quality printing technology and sophisticated counterfeiting techniques, the detection of fake currency has become increasingly challenging. In this article, we will explore how we can use <span style="color: cyan; font-weight: bold;">Convolutional Neural Networks (CNNs)</span> and <span style="color: cyan; font-weight: bold;">Streamlit</span> to develop a fake currency detection system that can detect fake currency with high accuracy.
+The first step in developing a fake currency detection system is to collect a dataset of currency images. We will need to collect images of both real and fake currency notes to train our CNN model. We can use web scraping techniques to collect images of real currency notes from various sources, such as the websites of central banks, financial institutions, and government agencies. For fake currency notes, we can simulate counterfeiting techniques to generate images that resemble real currency notes. We can also use publicly available datasets of fake currency notes.
+Once we have collected our dataset of currency images, we can begin to train our CNN model. We can use popular deep learning libraries such as <span style="color: yellow; font-weight: bold;">TensorFlow</span> or <span style="color: yellow; font-weight: bold;">PyTorch</span> to develop and train our model. We will need to pre-process our images by resizing them to a uniform size and normalizing the pixel values to improve the performance of our model.
+We can use a variety of CNN architectures such as <span style="color: yellow; font-weight: bold;">VGG, ResNet,</span> or <span style="color: yellow; font-weight: bold;">Inception</span> to develop our model. These architectures have been proven to be highly effective for image classification tasks. We can fine-tune these pre-trained models on our dataset of currency images using transfer learning. Transfer learning involves using a pre-trained model as a starting point and fine-tuning it on a new task by updating the weights of the last few layers of the model.
+After training our CNN model, we can evaluate its performance on a separate validation dataset to determine its accuracy. We can also use techniques such as data augmentation to improve the performance of our model by generating additional training data. Data augmentation involves applying transformations such as rotations, flips, and shears to our training images to create new variations of the original images.
+Once we have developed a CNN model that can accurately detect fake currency notes, we can integrate it into a Streamlit web application. Streamlit is a powerful tool for building data science applications and data visualizations with Python. We can use Streamlit to develop a user-friendly web application that allows users to upload images of currency notes and get real-time predictions of whether they are real or fake.
+To develop our Streamlit app, we will need to create a user interface that allows users to upload their images and view the model predictions. We can use Streamlit widgets such as file_uploader and image to allow users to upload their images and display the predictions. We can also add additional features such as a histogram of pixel values and an explanation of how the model works to enhance the user experience.
+In summary, fake currency detection is an important task that can be tackled using CNNs and Streamlit. By collecting a dataset of currency images, training a CNN model on this dataset, and integrating it into a Streamlit web application, we can develop a powerful tool for detecting fake currency with high accuracy. With the increasing availability of high-quality printing technology and sophisticated counterfeiting techniques, the need for such tools has never been greater. <a href="https://ieeexplore.ieee.org/document/8994968)" style="color: lime; font-weight: bold; text-decoration:none;">Know More</a></p>
+    '''
+    
+    st.markdown(para, unsafe_allow_html=True)
+    
+    
+    
+menu = ["Upload Image","About","Signup","Login", "Logout"]
 if 'session_id' not in st.session_state:
-    choice = st.sidebar.selectbox("Select an option", menu[1:-1])
+    choice = st.sidebar.selectbox("Select an option", menu[2:-1])
 else:
     choice = st.sidebar.selectbox("Select an option", menu)
     
@@ -113,5 +140,7 @@ elif choice == "Logout":
     logout()
 elif choice == "Upload Image":
     get_image()
+elif choice == "About":
+    about()
 else:
     st.write("Welcome back, " + st.session_state['session_id'])
